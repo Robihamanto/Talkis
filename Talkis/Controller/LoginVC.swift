b61ba19e817ff0f854578adae5792222f60add10
robihamanto <rrobie44@gmail.com>
2017-10-22 23:35:39 +0700

    added login view

---
+++ Talkis/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Talkis
+//
+//  Created by Robihamanto on 22/10/17.
+//  Copyright © 2017 bcc. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeButtonDidTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ Talkis/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Talkis
+//
+//  Created by Robihamanto on 22/10/17.
+//  Copyright © 2017 bcc. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
