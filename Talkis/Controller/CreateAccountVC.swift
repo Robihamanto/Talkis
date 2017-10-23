f2811efd68fd13558ddfceca8e5fbe5033db715a
robihamanto <rrobie44@gmail.com>
2017-10-23 16:26:16 +0700

    registered done

--- Talkis/Controller/CreateAccountVC.swift
+++ Talkis/Controller/CreateAccountVC.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,5 +26,31 @@ class CreateAccountVC: UIViewController {
     @IBAction func closeButtonDidTap(_ sender: Any) {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
     }
+    
+    @IBAction func createAccountDidTap(_ sender: Any) {
+        guard let email = emailText.text, emailText.text != "" else
+        {
+            return
+        }
+        guard let password = passwordText.text, passwordText.text != "" else
+        {
+            return
+        }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("User registered!")
+            }
+        }
+        
+    }
+    
+    @IBAction func chooseAvatarDidTap(_ sender: Any) {
+    }
+    
+    @IBAction func generateAvatarDidTap(_ sender: Any) {
+    }
+    
+    
     
 }
